5095eec19a8a40148de2f7b2ab3313d371702181
Joao Victor Negreiros <joaovictornsv@gmail.com>
2023-06-14 23:46:11 -0300

    add recognition on create log

--- create_log/__init__.py
+++ create_log/__init__.py
@@ -4,25 +4,30 @@ import logging
 from lib.database import get_logs_container
 from lib.json import to_json
 from lib.storage import upload_blob
+from lib.face_recognition import check_user_permission
 import re
 from datetime import datetime
 
 
-def create_log_use_case(data):
+def create_log_use_case(req_data):
     logs_container = get_logs_container()
+    data_to_save = dict()
+    data_to_save["id"] = str(uuid.uuid4())
 
-    data["id"] = str(uuid.uuid4())
-
-    image_base64 = data["image"]
     filename = upload_blob(
-        data=image_base64,
-        filename=data["id"],
+        data=req_data["image"],
+        filename=data_to_save["id"],
         container="access"
     )
-    data["image"] = filename
-    data["created_at"] = str(datetime.now())
+    data_to_save["image"] = filename
 
-    user = logs_container.create_item(data)
+    user_info = check_user_permission(req_data["image"])
+
+    data_to_save["user_name"] = user_info["user_name"]
+    data_to_save["allowed"] = user_info["allowed"]
+    data_to_save["created_at"] = str(datetime.now())
+
+    user = logs_container.create_item(data_to_save)
 
     return user
 
